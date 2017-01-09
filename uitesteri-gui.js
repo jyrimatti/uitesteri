8e1ed2064c9eeea5e2d83574836bbe8b079c77e9
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2017-01-09 22:51:23 +0200

    simplify initialization process, pull dependencies from internet

--- uitesteri-gui.js
+++ uitesteri-gui.js
@@ -1,6 +1,22 @@
 
 $(function() {
-    var testframe = document.getElementById('testframe');
+    window.onmessage = function(original) { return function(e) {
+        console.debug('uitesteri-gui received message: ');
+        console.debug(e);
+        if (e.data.name == 'load') {
+            postMessage({ name: 'init', url: 'https://cdnjs.cloudflare.com/ajax/libs/yui/3.18.1/yui-base/yui-base-min.js' }, '*');
+            setTimeout(function() {
+                postMessage({ name: 'init', url: 'https://cdnjs.cloudflare.com/ajax/libs/yui/3.18.1/event-simulate/event-simulate-min.js' }, '*');
+            }, 25); // why do I need the delay?
+            setTimeout(function() {
+                var basepath = window.location.href.substring(0, window.location.href.replace(window.location.hash, '').lastIndexOf('/') + 1);
+                postMessage({ name: 'init', url: basepath + 'uitesteri-runner.js' }, '*');
+            }, 50); // why do I need the delay?
+        }
+        if (original) {
+            original(e);
+        }
+    };}(window.onmessage);
 
     if (window.location.hash.length <= 1) {
         window.location.hash = 'https://raw.githubusercontent.com/jyrimatti/uitesteri/master/demosuite.js';
@@ -18,6 +34,13 @@ $(function() {
         runall();
     }
 });
+
+var postMessage = function(data) {
+    var testframe = document.getElementById('testframe');
+    console.debug('uitesteri-gui posting message:');
+    console.debug(data);
+    testframe.contentWindow.postMessage(data, '*');
+};
 
 var addSuite = function(suite) {
     var suiteHTML = $('<div class="suite"><h3>' + (suite.name ? suite.name : 'unnamed suite') + '</h3><div class="content"><button class="runSuite" onclick="runSuite(this)">aja</button><div class="suitetests"></div></div></div>').appendTo($('.tests'));
@@ -41,7 +64,7 @@ var runall = function() {
 };
 
 var step = function() {
-    document.getElementById('testframe').contentWindow.postMessage({ name: 'continue' }, '*');
+    postMessage({ name: 'continue' }, '*');
 };
 
 var run = function(tests) {
@@ -58,7 +81,7 @@ var run = function(tests) {
 };
 
 var finished = function(test, results) {
-    console.log(results);
+    console.info(results);
     $(test).filter(function() {
         return $('pre', this).text() == results.test;
     }).addClass(succeeded(results) ? 'success' : 'failed')
@@ -77,8 +100,8 @@ var resetNewTest = function() {
 
 window.speedChange = function(self) {
     var speed = self.value;
-    console.log('Changing speed to ' + speed);
-    testframe.contentWindow.postMessage({ name: 'speed', speed: speed }, '*');
+    console.info('Changing speed to ' + speed);
+    postMessage({ name: 'speed', speed: speed }, '*');
 };
 
 window.runTest = function(self) {
@@ -116,25 +139,29 @@ var runIn = function(iframe, test, callback) {
         throw new Error('Already executing');
 
     var doRun = function(skip) {
-        iframe.contentWindow.postMessage({ name: 'speed', speed: document.getElementById('speed').value }, '*');
-        iframe.contentWindow.postMessage({ name: 'run', test: test.toString(), skip: skip }, '*');
+        postMessage({ name: 'speed', speed: document.getElementById('speed').value }, '*');
+        postMessage({ name: 'run', test: test.toString(), skip: skip }, '*');
     };
 
     var executedResults = [];
     var newHandler = function(e) {
+        if (originalHandler) {
+            originalHandler(e);
+        }
+
         if (e.data.name == 'finished') {
             window.onmessage = originalHandler;
             var allResults = e.data.results;
             allResults.results = executedResults.concat(allResults.results);
             callback({test: test, results: allResults });
         } else if (e.data.name == 'commandExecuted') {
-            console.log(e.data.result);
+            console.debug(e.data.result);
             executedResults.push(e.data.result);
         } else if (e.data.name == 'load') {
-            doRun(executedResults.length);
+            setTimeout(function() {
+                doRun(executedResults.length);
+            }, 75);  // why do I need the delay?
         }
-        if (originalHandler)
-            originalHandler(e);
     };
     newHandler.uitestHandler = true;
     window.onmessage = newHandler;
